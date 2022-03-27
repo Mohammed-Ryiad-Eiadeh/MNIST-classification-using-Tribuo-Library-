2ec488d60d336b3e94b54d404343a069344b0275
Mohammed Al-Eiadeh <93108547+Mohammed-Ryiad-Eiadeh@users.noreply.github.com>
2022-03-28 01:44:34 +0300

    Delete ML_With_Interface directory

--- ML_With_Interface/src/main/java/Interface/ML/AllProcesses.java
+++
@@ -1,9 +0,0 @@
-package Interface.ML;
-
-public interface AllProcesses {
-    void GenerateDataSource();
-    void TrainClassifier();
-    void EvaluateClassifier();
-    void DisplaySummary();
-    double GetAccuracy();
-}
